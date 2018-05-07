538f524447885c39c8fafe8bc30ece1363f2be49
Marc Kassay <marckassay@gmail.com>
2018-05-07 17:21:10 -0400

    stash to work on profile form .ps1 to .json

--- resources/MK.PowerShell-config.ps1
+++ resources/MK.PowerShell-config.ps1
@@ -1,8 +1,14 @@
 $MKPowerShellConfig = @{ 
     LastLocation               = ''
     NuGetApiKey                = ''
+    BackupLocations            = @{
+        Path        = @()
+        Destination = ''
+    }
     TurnOnAvailableUpdates     = 'true'
     TurnOnHistoryRecording     = 'true'
     TurnOnQuickRestart         = 'true'
     TurnOnRememberLastLocation = 'true'
+    TurnOnBackup               = 'true'
+    BackupPolicy               = 'Auto'
 }
--- src/settings/Set-MKPowerShellSetting.ps1
+++ src/settings/Set-MKPowerShellSetting.ps1
@@ -23,79 +23,37 @@ function Set-MKPowerShellSetting {
     }
 
     end {
-        # cast to bool, make all chars lowercase...
-        if ($Value -match "^(T|t)rue|(F|f)alse$") {
-            $StringValue = $Value.ToLower()
-        }
-        else {
-            $StringValue = $Value
-        }
+        if ($Value) {
+            # cast to bool, make all chars lowercase...
+            if ($Value -match "^(T|t)rue|(F|f)alse$") {
+                $StringValue = $Value.ToLower()
+            }
+            else {
+                $StringValue = $Value
+            }
         
-        $EntryValue = @"
+            $EntryValue = @"
     $Name = '$StringValue'
 "@
 
-        $ModifiedContent = (Get-Content -Path $ConfigFilePath) | `
-            ForEach-Object {$_ -Replace "^\s*$Name(\s*\=\s*)[\w\W]*", $EntryValue}
-        Set-Content -Path $ConfigFilePath -Value $ModifiedContent -PassThru:$PassThru.IsPresent
+            $ModifiedContent = (Get-Content -Path $ConfigFilePath) | `
+                ForEach-Object {$_ -Replace "^\s*$Name(\s*\=\s*)[\w\W]*", $EntryValue}
+            Set-Content -Path $ConfigFilePath -Value $ModifiedContent -PassThru:$PassThru.IsPresent
+        }
+        <#         else if ($Data) {
+            (Get-Content -Path $ConfigFilePath)
+            Invoke-Command -ScriptBlock {
+            .\marckassay\MK.PowerShell\MK.PowerShell.4PS\resources\MK.PowerShell-config.ps1
+            $MKPowerShellConfig.BackUpLocations = $Data
+            }
+        } #>
 
         switch ($Name) {
             TurnOnRememberLastLocation { Restore-RememberLastLocation }
             TurnOnQuickRestart { Restore-QuickRestartSetting }
             #TurnOnHistoryRecording { Restore-TurnOnHistoryRecording }
+            #TurnOnBackup
             Default {}
-        }
-    }
-}
-
-# NoExport: Restore-RememberLastLocation
-function Restore-RememberLastLocation {
-    [CmdletBinding()]
-    Param(
-        [switch]$Initialize
-    )
-
-    if ((Get-MKPowerShellSetting -Name 'TurnOnRememberLastLocation') -eq $true) {
-        Set-Alias sl Set-LocationAndStore -Scope Global -Force
-        Write-Host "'sl' alias is now mapped to 'Set-LocationAndStore'." -ForegroundColor Green
-
-        if ($Initialize.IsPresent) {
-            $LastLocation = Get-MKPowerShellSetting -Name 'LastLocation'
-            if ($LastLocation -ne '') {
-                Set-LocationAndStore -Path $LastLocation
-            }
-            else {
-                Set-LocationAndStore -Path $(Get-Location)
-            }
-        }
-    }
-    else {
-        Set-Alias sl Set-Location -Scope Global -Force
-        Write-Host "'sl' alias is now mapped to 'Set-Location'." -ForegroundColor Green
-    }
-}
-
-# NoExport: Restore-QuickRestartSetting
-function Restore-QuickRestartSetting {
-    [CmdletBinding()]
-    Param(
-        [switch]$Initialize
-    )
-
-    if ((Get-MKPowerShellSetting -Name 'TurnOnQuickRestart') -eq $true) {
-        Set-Alias pwsh Restart-PWSH -Scope Global
-        Write-Host "'pwsh' alias is now mapped to 'Restart-PWSH'." -ForegroundColor Green
-
-        Set-Alias pwsha Restart-PWSHAdmin -Scope Global
-        Write-Host "'pwsha' alias is now mapped to 'Restart-PWSHAdmin'." -ForegroundColor Green
-    }
-    else {
-        if (-not $Initialize.IsPresent) { 
-            Remove-Alias pwsh -Scope Global
-            Write-Host "'pwsh' alias has been removed." -ForegroundColor Green
-
-            Remove-Alias pwsha -Scope Global
-            Write-Host "'pwsha' alias has been removed." -ForegroundColor Green
         }
     }
 }
